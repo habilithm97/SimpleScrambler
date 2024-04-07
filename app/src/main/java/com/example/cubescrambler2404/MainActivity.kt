ab1178bb945ecb4e183a7bcdb9506be237a995f9
habilithm97 <habinkim97@gmail.com>
2024-04-08 00:16:36 +0900

    메인 UI

--- app/src/main/java/com/example/cubescrambler2404/MainActivity.kt
+++ app/src/main/java/com/example/cubescrambler2404/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.cubescrambler2404
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cubescrambler2404.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    private fun init() {
+
     }
 }
