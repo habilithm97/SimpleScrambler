a8ba32fa4b5716ba87d73a4c953612cc774d7a3f
habilithm97 <habinkim97@gmail.com>
2024-04-07 23:48:35 +0900

    Initial commit

---
+++ app/src/main/java/com/example/cubescrambler2404/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cubescrambler2404
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
